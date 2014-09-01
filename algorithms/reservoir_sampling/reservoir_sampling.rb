aa9fdcbb536627bb69af746c550a1ddfdff85f6e
Alex <a.aleksu@gmail.com>
2014-09-01 10:59:49 +0700

    Added comment

--- algorithms/reservoir_sampling/reservoir_sampling.rb
+++ algorithms/reservoir_sampling/reservoir_sampling.rb
@@ -1,3 +1,7 @@
+# Reservoir sampling is a family of randomized algorithms 
+# for randomly choosing a sample of k items from a list S 
+# containing n items, where n is either a very large or unknown number
+# https://en.wikipedia.org/wiki/Reservoir_sampling
 def reservoir_sampling a, k
   reservoir = Array.new k
   0.upto(k) do |i|
