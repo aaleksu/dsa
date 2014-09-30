8a06ea0e0fc042e0d67aaa6acd396d7daac3c2be
Alex <a.aleksu@gmail.com>
2014-09-30 13:17:06 +0700

    added alternative approach with matrix multiplication; refactoring

--- algorithms/fibonacci/fibonacci.rb
+++ algorithms/fibonacci/fibonacci.rb
@@ -1,29 +1,69 @@
-def fibonacci n
-  p "Calculating Fibonacci number for %d" % n
-  start_time = Time.now
-  a = b = 1
+require 'matrix'
 
-  3.upto(n) do
-    c = a + b
-    a = b
-    b = c
+class ArgumentsValidator
+  def self.validate
+    raise Exception.new "\nGive me a number\n" if ARGV.empty?
+    raise Exception.new "\nArgument must be an integer\n" unless ARGV.first =~ /\d+/
+  end
+end
+
+class Fibonacci
+
+  def initialize n
+    @n = n.to_i
   end
 
-  exec_time = (Time.now - start_time) * 1000.0
+  def run
+    start_one = Time.now
+    puts "\nCalculation Fibonacci number for %d with transitional values" % @n
+    self.with_transitional_values
 
-  p "Total executing time is %s" % exec_time
+    mark_exec_time start_one
 
-  b
+    start_two = Time.now
+    puts "Calculation Fibonacci number for %d with matrices" % @n
+    self.with_matrices
+    mark_exec_time start_two
+  end
+
+  # more efficient if n ~< 1500
+  def with_transitional_values
+    a = b = 1
+    3.upto(@n) do
+      c = a + b
+      a = b
+      b = c
+    end
+
+    #puts "answer is %d" % b
+  end
+
+  # more efficient since n ~ 1500
+  def with_matrices
+    @m = Matrix.identity 2
+    self.math_pow(@n - 1)
+
+    #puts "answer is %d" % @m.element(0, 0)
+  end
+
+  def math_pow(n_int)
+    if n_int > 1
+      self.math_pow n_int/2
+      @m = @m * @m
+    end
+
+    @m = @m * Matrix[[1, 1], [1, 0]] if n_int % 2 != 0
+  end
+
+  def mark_exec_time marked_time
+    exec_time = (Time.now - marked_time) * 1000.0
+    puts "Total executing time is %s\n\n" % exec_time
+  end
 end
 
-def validate_input_arguments
-  raise Exception.new "\nGive me a number\n" if ARGV.empty?
-  raise Exception.new "\nArgument must be an integer\n" unless ARGV.first =~ /\d+/
-end
-
-validate_input_arguments
+ArgumentsValidator.validate
 
 num = ARGV.first
 
-p fibonacci num.to_i
-
+fibonacci_instance = Fibonacci.new num
+fibonacci_instance.run
